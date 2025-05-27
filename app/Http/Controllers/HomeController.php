89ed0439c78169227aeae3f6990b68aee513071e
AlvinaDamayanti123 <damayantialfina45@gmail.com>
2025-05-28 00:39:51 +0700

    membuat fungsi fitur data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,39 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index ()
+    private function getFiturData()
     {
-        return view ('home');
+        return [
+            [
+                'image' => 'Fitur1.png',
+                'title' => 'Fasilitas',
+                'type' => 'Rumah minimalis Type-A2',
+                'price' => 'IDR.200jt'
+            ],
+            [
+                'image' => 'Fitur1.png',
+                'title' => 'Kamar Tidur',
+                'type' => 'Rumah minimalis Type-A2',
+                'price' => 'IDR.200jt'
+            ],
+            [
+                'image' => 'Fitur1.png',
+                'title' => 'Kamar Tidur',
+                'type' => 'Rumah minimalis Type-A2',
+                'price' => 'IDR.200jt'
+            ],
+            [
+                'image' => 'Fitur1.png',
+                'title' => 'Kamar Tidur',
+                'type' => 'Rumah minimalis Type-A2',
+                'price' => 'IDR.200jt'
+            ]
+        ];
+    }
+
+    public function index()
+    {
+        $fiturData = $this->getFiturData();
+        return view('home', compact('fiturData'));
     }
 }
